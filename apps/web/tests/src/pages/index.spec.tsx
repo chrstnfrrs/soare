2ea8b53d3d45709f3b0cea4f2319a2cde503da18
Christian Farris <christianfarriscs@gmail.com>
2022-04-04 00:39:28 -0500

    fix: format

--- apps/web/tests/src/pages/index.spec.tsx
+++ apps/web/tests/src/pages/index.spec.tsx
@@ -6,18 +6,17 @@ import Home from '../../../src/pages/index';
 vi.mock('@apollo/client', () => ({
   ApolloClient: vi.fn(),
   HttpLink: vi.fn(),
-}))
+}));
 
 vi.mock('../../../src/graphql/adapters/client', () => ({
   client: vi.fn(),
   query: vi.fn(),
-}))
+}));
 
 describe('Given <Home />', () => {
   test('Then should display landing page', () => {
-    render(<Home users={{}}/>);
+    render(<Home users={{}} />);
 
     expect(screen.getByText('Hello World!')).toBeVisible();
   });
 });
-
